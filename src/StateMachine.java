4f21802d5fb7ba71dbcd3a502fee86138f03e228
kgk <k.gkoutzis@imperial.ac.uk>
2018-11-10 20:53:18 +0000

    Removed UTF8 tests

--- src/StateMachine.java
+++ src/StateMachine.java
@@ -1,59 +1,57 @@
-
 public class StateMachine {
 
-    public static boolean binMachine(String input) {
-        // TODO
-        return false;
-    }
+  public static boolean binMachine(String input) {
+    // TODO
+    return false;
+  }
 
-    public static int binConvMachine(String input) {
-        // TODO
-        return -1;
-    }
+  public static int binConvMachine(String input) {
+    // TODO
+    return -1;
+  }
 
-    public static boolean numMachine(String input) {
-        // TODO
-        return false;
-    }
+  public static boolean numMachine(String input) {
+    // TODO
+    return false;
+  }
 
-    public static boolean decMachine(String input) {
-        // TODO
-        return false;
-    }
+  public static boolean decMachine(String input) {
+    // TODO
+    return false;
+  }
 
-    public static boolean signMachine(String input) {
-        // TODO
-        return false;
-    }
+  public static boolean signMachine(String input) {
+    // TODO
+    return false;
+  }
 
-    public static boolean numberMachine(String input) {
-        // TODO
-        return false;
-    }
+  public static boolean numberMachine(String input) {
+    // TODO
+    return false;
+  }
 
-    public static boolean wordMachine(String input) {
-        // TODO
-        return false;
-    }
+  public static boolean wordMachine(String input) {
+    // TODO
+    return false;
+  }
 
-    public static boolean wordyMachine(String input) {
-        // TODO
-        return false;
-    }
+  public static boolean wordyMachine(String input) {
+    // TODO
+    return false;
+  }
 
-    public static boolean sentenceMachine(String input) {
-        // TODO
-        return false;
-    }
+  public static boolean sentenceMachine(String input) {
+    // TODO
+    return false;
+  }
 
-    public static boolean grammarMachine(String input) {
-        // TODO
-        return false;
-    }
+  public static boolean grammarMachine(String input) {
+    // TODO
+    return false;
+  }
 
-    public static double calcMachine(String input) {
-        // TODO
-        return Double.NaN;
-    }
+  public static double calcMachine(String input) {
+    // TODO
+    return Double.NaN;
+  }
 }
-
